0d0ec33baf84a76707a89b2ec8bbcef1c79ee829
SebLdn705 <sebastien.monfort.ldn@gmail.com>
2021-03-25 00:20:48 +0000

    commit api

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # OC_Project_5
-OC Project with the aim to create an auto tag suggestion based on ML technics
+OC Project with the aim to create an auto tag suggestion based on ML Technics. The query is made via a Flask API.
+
+
+##**Table of contents**
+
+* [General Info](#general-info)
+* [Technologies](#technologies)
+* [How to install](#how-to-install)
+* [ML Model](#ML_Model_and_assumptions_/_limitations_of_the_model)
+
+## General Info
+
+The API retrieves the most likely tags to put according to the question/title of a stackoverflow query. 
+It returns a JSON file containing the tags.
+
+### ML Model and assumptions / limitations of the model
+
+The model is based on a supervised model using the SVM SVC and training with a f1-micro scoring. The possible number of
+tags was limited to the top 100 tags used. If no tag is retrieved it is maybe best to try to reformulate the question in
+a better manner by using more keywords (programming specific language)
+
+
+### How to install
+
+Clone the repository using git clone https://github.com/SebLdn705/OC_Project_5.git
+
+In order to run the model from a jupyter notebook, you'll need to run the python api.py. The server should be working 
+http://127.0.0.1:5000/predict
+
+### How to pass the question to the API
+
+The API requires the question to be sent under a JSON format. For instance, to get the predicted tags it is recommended 
+to send the question with the below format (example from a Jupyter Notebook)
+
+import requests
+
+URL = 'http://127.0.0.1.5000/predict'
+
+params = {'question': 'Your stack overflow question/Title'} -> format is under a dictionary type
+
+r = requests.post(url=URL, json=params)
+
+question = r.json()
+
+### Technologies
+
+The API is running with:
+* nltk: 3.5
+* flask: 1.1.2
+* joblib: 0.14.1
+* pandas: 1.0.3
+
+
+
+
--- api.py
+++ api.py
@@ -9,61 +9,30 @@ app = Flask(__name__)
 # start the debugger - if code is not correct than an error will appear when the app is visited
 app.config["DEBUG"] = True
 
+model_path = "models/stack_overflow_tag_prediction.joblib"
+target_path = "models/target_col.joblib"
+model = load(model_path)
+target_col = load(target_path)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        try:
-            json_ = request.json
-            data = json_
 
-            model_path = "models/stack_overflow_tag_prediction.joblib"
-            target_path = "models/target_col.joblib"
-            model = load(model_path)
-            target_col = load(target_path)
-
-            output = []
-            question = data['question']
-            processed_text = text_preprocess(question)
-            output.append(' '.join(processed_text))
-
-            result = models.prediction(model, 0.35, output, target_col)
-
-            return jsonify({"prediction": result})
-            # return json_
-        except:
-            return jsonify({"trace": traceback.format_exc()})
-
-
-# routing >> mapping url to function - mapped to path '/'
-# methods list is a keyword that lets Flask know what type of request is allowed
-# type of request for the web applications:
-#  - GET to send data from the application to the user
-#  - POST to receive data from the user
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-
-        model_path = "models/stack_overflow_tag_prediction.joblib"
-        target_path = "models/target_col.joblib"
-        model = load(model_path)
-        target_col = load(target_path)
+    try:
+        json_ = request.json
+        data = json_
 
         output = []
-        question = request.form.get('question')
+        question = data['question']
         processed_text = text_preprocess(question)
         output.append(' '.join(processed_text))
 
         result = models.prediction(model, 0.35, output, target_col)
-    return render_template('index.html', prediction=result)
-    # return render_template('index.html')
-#
 
-# # A route to return all of the available entries in our catalog.
-# @app.route('/test', methods=['GET'])
-# def api_all():
-#     return jsonify(meta)
+        return jsonify({"prediction": result})
+    except:
+        return jsonify({"trace": traceback.format_exc()})
+
 
 if __name__ == "__main__":
-
     app.run()  # run the application server
